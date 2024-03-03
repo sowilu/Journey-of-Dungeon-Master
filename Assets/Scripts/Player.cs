97ffe51b34c9c4d01ac3138c3f373de89fe4da71
Simona <sima.simute1999@gmail.com>
2024-03-03 11:16:23 +0200

    Bullet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,18 @@ public class Player : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public float speed = 5;
-    
-    Rigidbody2D rb;
+    public float cooldown = 0.1f;
+
+    private Action shoot;
+    private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Vector3 shootInput;
+    private float lastShot = 0;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        shoot = ShootBullet;
     }
     
     void Update()
@@ -20,19 +26,40 @@ public class Player : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
-        var shootInput = new Vector3();
+        shootInput = new Vector3();
         shootInput.x = Input.GetAxis("HorizontalArrow");
         shootInput.y = Input.GetAxis("VerticalArrow");
 
-        if(shootInput != Vector3.zero)
+        if(shootInput != Vector3.zero && lastShot + cooldown <= Time.time)
         {
-            Instantiate(bulletPrefab, transform.position + shootInput, Quaternion.identity);
-        }
+            lastShot = Time.time;
 
+            shoot();
+        }
+    }
+
+    void ShootBullet()
+    {
+        var bullet = Instantiate(bulletPrefab, transform.position + shootInput.normalized, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = shootInput.normalized * speed * 3;
+
+        Destroy(bullet, 2);
     }
     
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveInput.normalized * speed * Time.fixedDeltaTime);
+    }
+
+    public void ChangeShoot(Action newShoot, float duration)
+    {
+        StartCoroutine(ChangeShootCoroutine(newShoot, duration));
+    }
+
+    IEnumerator ChangeShootCoroutine(Action newShoot, float duration)
+    {
+        shoot = newShoot;
+        yield return new WaitForSeconds(duration);
+        shoot = ShootBullet;
     }
 }
